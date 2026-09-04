87d96991cbcf21017cc82f283899cecbbc66ca34
Minh Tran <tranphuongminh2346@gmail.com>
2026-09-04 14:50:06 +0800

    Time: 7 ms (41.22%), Space: 19.4 MB (20.2%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,13 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        # Thay 6 trường hợp đặc biệt thành chuỗi cộng thuần túy
+        s = s.replace("IV", "IIII").replace("IX", "VIIII") \
+             .replace("XL", "XXXX").replace("XC", "LXXXX") \
+             .replace("CD", "CCCC").replace("CM", "DCCCC")
+        
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        current_char = 0
-
-        for char in reversed(s):
-            if roman_map[char] < current_char:
-                total -= roman_map[char]
-            else:
-                total += roman_map[char]
-            current_char = roman_map[char]
-        return total
+        
+        # Hàm sum() chạy bằng mã C tích hợp sẵn nên cực kỳ nhanh
+        return sum(roman_map[char] for char in s)
