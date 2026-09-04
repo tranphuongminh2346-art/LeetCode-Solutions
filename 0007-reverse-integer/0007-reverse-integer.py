a1097105d17cca8ae39c57024bd9d6a584aeebe9
Minh Tran <tranphuongminh2346@gmail.com>
2026-09-04 15:35:09 +0800

    Time: 46 ms (57.13%), Space: 19.2 MB (32.06%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,7 +3,12 @@ class Solution:
         scale = 1
         if x < 0:
             scale = -1
-        reversed_x = int(str(abs(x))[::-1]) * scale
+        x = abs(x)
+        reversed_x = 0
+        while x > 0:
+            reversed_x = reversed_x * 10 + x%10
+            x//=10
+        reversed_x *= scale
         
         if -2**31 <= reversed_x <= 2**31 - 1:
             return reversed_x
