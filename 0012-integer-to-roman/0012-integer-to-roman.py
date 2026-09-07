41a4ae4ebd792d9656004893f8be0ba572422eff
Minh Tran <tranphuongminh2346@gmail.com>
2026-09-07 13:49:11 +0800

    Time: 7 ms (38.21%), Space: 19.4 MB (27%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,23 +1,7 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # Danh sách các cặp (giá trị, ký hiệu La Mã) sắp xếp giảm dần
-        roman_mapping = [
-            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
-            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
-            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
-            (1, "I")
-        ]
-        
-        result = []
-        
-        # Duyệt qua từng cặp giá trị và ký hiệu
-        for value, symbol in roman_mapping:
-            if num == 0:
-                break
-            # Tính số lần ký hiệu này xuất hiện (ví dụ 3000 // 1000 = 3 lần 'M')
-            count = num // value
-            if count > 0:
-                result.append(symbol * count)
-                num %= value # Lấy phần dư còn lại để tính tiếp
-                
-        return "".join(result)
+        m = ["", "M", "MM", "MMM"]
+        c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return f"{m[num // 1000]}{c[(num % 1000) // 100]}{x[(num % 100) // 10]}{i[num % 10]}"
