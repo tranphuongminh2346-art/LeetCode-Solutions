ac4fcfcc7fbcdc11011c2290bbb6752d57c5c626
Minh Tran <tranphuongminh2346@gmail.com>
2026-09-10 18:13:13 +0800

    Time: 3 ms (99.71%), Space: 19.5 MB (15.59%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,28 +1,41 @@
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
         nums.sort()
-        n = len(nums)
-        # Khởi tạo đáp án bằng tổng của 3 phần tử đầu tiên
-        closest_sum = nums[0] + nums[1] + nums[2]
+        return self.kSumClosest(nums, 3, target)
+
+    def kSumClosest(self, nums: list[int], k: int, target: int) -> int:
         
-        for i in range(n - 2):
+        if len(nums) == k:
+            return sum(nums)
+
+     
+        smallest_sum = sum(nums[:k])
+        if smallest_sum >= target:
+            return smallest_sum
+
+        largest_sum = sum(nums[-k:])
+        if largest_sum <= target:
+            return largest_sum
+
+        if k == 1:
+            return min(nums, key=lambda x: abs(target - x))
+
+        closest_sum = smallest_sum
+        
+        for i in range(len(nums) - k + 1):
+
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-                
-            j, k = i + 1, n - 1
-            
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
-                
-                if total == target:
-                    return total
-                
-                if abs(total - target) < abs(closest_sum - target):
-                    closest_sum = total
-                
-                if total < target:
-                    j += 1
-                else:
-                    k -= 1
-                    
+
+
+            remainder_closest = self.kSumClosest(nums[i + 1:], k - 1, target - nums[i])
+            current_sum = nums[i] + remainder_closest
+
+
+            if abs(target - current_sum) < abs(target - closest_sum):
+                closest_sum = current_sum
+
+            if closest_sum == target:
+                return target
+
         return closest_sum
